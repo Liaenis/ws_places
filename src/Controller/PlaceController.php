f5a86d3c907e7c80787beca7f940ec04adf29958
Liaenis <84380297+Liaenis@users.noreply.github.com>
2021-08-26 14:56:19 +0200

    Update PlaceController.php

--- src/Controller/PlaceController.php
+++ src/Controller/PlaceController.php
@@ -6,7 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
-
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\Exception\NotEncodableValueException;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 use App\Repository\PlaceRepository;
 use App\Entity\Place;
 
@@ -33,4 +37,25 @@ class PlaceController extends AbstractController
         $reponse = new Response($json, 200, [ 'content-type' => 'application/json' ]); 
         return $reponse; 
     }
+    /** 
+* @Route("/api/place", name="api_place_add",methods="POST") 
+*/ 
+public function add(EntityManagerInterface $entityManager, Request $request, SerializerInterface $serializer, ValidatorInterface $validator)
+{ 
+    $contenu = $request->getContent(); 
+    try { $endroit = $serializer->deserialize($contenu, Place::class, 'json');
+       $errors = $validator->validate($endroit); 
+       if (count($errors) > 0) 
+       { 
+           return $this->json($errors, 400); 
+       } 
+       $entityManager->persist($endroit); 
+       $entityManager->flush(); 
+       return $this->json($endroit, 201, [], ['groups' => 'place:read']); 
+   } 
+   catch (NotEncodableValueException $e) 
+   { 
+       return $this->json(['status' => 400,'message' => $e->getMessage()]); 
+   } 
+}
 }
